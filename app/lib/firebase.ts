b5d32b985471d2c0460b570928c8c6467011ba2b
Shinaz Navas <shinaz.navas@gmail.com>
2025-05-19 15:14:28 +0100

    feat: firebase integration v1

---
+++ app/lib/firebase.ts
@@ -0,0 +1,17 @@
+// lib/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyBmsS8-TIPz0HRNxXDnmu73weBwQ3hn_1I",
+  authDomain: "niyyah-proto-v1.firebaseapp.com",
+  projectId: "niyyah-proto-v1",
+  storageBucket: "niyyah-proto-v1.firebasestorage.app",
+  messagingSenderId: "246353599180",
+  appId: "1:246353599180:web:22581acf82733b8889c069",
+};
+
+// Initialize and export Firestore
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
